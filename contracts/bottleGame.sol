2419f97c80c93d6df4afe4a7670c2bc0dd95c401
Ruth Uche <raymondruth12@gmail.com>
2025-02-19 20:16:11 +0100

    Updated code

--- contracts/bottleGame.sol
+++ contracts/bottleGame.sol
@@ -8,8 +8,8 @@ contract ColorBottleGame {
     address public owner;
 
     event GameStarted(uint256[5] sequence);
-    event AttemptMade(address player, uint8 correctPositions, uint8 remainingAttempts);
-    event GameWon(address winner);
+    event AttemptMade(address indexed player, uint8 correctPositions, uint8 remainingAttempts);
+    event GameWon(address indexed winner);
     event GameReset();
 
     modifier onlyOwner() {
@@ -22,22 +22,16 @@ contract ColorBottleGame {
         _startNewGame();
     }
 
-    function _generateRandomSequence() private view returns (uint256[5] memory) {
-        uint256 randomSeed = block.timestamp + block.prevrandao + uint256(uint160(msg.sender));
-        uint256 randomHash = uint256(keccak256(abi.encode(randomSeed)));
-
-        uint256[5] memory sequence;
+    function _generateRandomSequence() private view returns (uint256[5] memory sequence) {
+        uint256 randomSeed = uint256(keccak256(abi.encode(block.timestamp, block.prevrandao, msg.sender)));
         uint256 availableNumbers = 0x12345;
 
         for (uint8 i = 0; i < 5; i++) {
-            uint8 index = uint8(randomHash % (5 - i));
-            uint8 num = uint8((availableNumbers >> (index * 4)) & 0xF);
-            sequence[i] = num;
+            uint8 index = uint8(randomSeed % (5 - i));
+            sequence[i] = (availableNumbers >> (index * 4)) & 0xF;
             availableNumbers &= ~(uint256(0xF) << (index * 4));
-            randomHash /= 5;
+            randomSeed /= 5;
         }
-
-        return sequence;
     }
 
     function _startNewGame() internal {
@@ -47,11 +41,10 @@ contract ColorBottleGame {
         emit GameStarted(correctSequence);
     }
 
-    function makeAttempt(uint256[5] memory playerGuess) external returns (uint8) {
+    function makeAttempt(uint256[5] calldata playerGuess) external returns (uint8 correctPositions) {
         require(gameActive, "Game is not active");
         require(attempts > 0, "No attempts left");
 
-        uint8 correctPositions = 0;
         for (uint8 i = 0; i < 5; i++) {
             if (playerGuess[i] == correctSequence[i]) {
                 correctPositions++;
@@ -68,7 +61,6 @@ contract ColorBottleGame {
         }
 
         emit AttemptMade(msg.sender, correctPositions, attempts);
-        return correctPositions;
     }
 
     function resetGame() external onlyOwner {
@@ -76,7 +68,7 @@ contract ColorBottleGame {
         emit GameReset();
     }
 
-    function getGameStatus() external view returns (uint256[5] memory, uint8, bool) {
+    function getGameStatus() external view returns (uint256[5] memory sequence, uint8 remainingAttempts, bool isActive) {
         return (correctSequence, attempts, gameActive);
     }
 }
--- scripts/interact.ts
+++ scripts/interact.ts
@@ -1,20 +1,20 @@
-// import { ethers } from "hardhat";
+import { ethers } from "hardhat";
 
-// async function main() {
-//     const [deployer] = await ethers.getSigners();
-//     const gameAddress = "DEPLOYED_CONTRACT_ADDRESS";
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const gameAddress = "DEPLOYED_CONTRACT_ADDRESS";
 
-//     const ColorBottleGame = await ethers.getContractAt("ColorBottleGame", gameAddress);
+    const ColorBottleGame = await ethers.getContractAt("ColorBottleGame", gameAddress, deployer);
 
-//     console.log("Game Status:", await ColorBottleGame.getGameStatus());
+    console.log("Game Status:", await ColorBottleGame.getGameStatus());
 
-//     const attemptTx = await ColorBottleGame.makeAttempt([1, 2, 3, 4, 5]);
-//     await attemptTx.wait();
+    const attemptTx = await ColorBottleGame.makeAttempt([1, 2, 3, 4, 5]);
+    await attemptTx.wait();
 
-//     console.log("Updated Game Status:", await ColorBottleGame.getGameStatus());
-// }
+    console.log("Updated Game Status:", await ColorBottleGame.getGameStatus());
+}
 
-// main().catch((error) => {
-//     console.error(error);
-//     process.exit(1);
-// });
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
